351ebb1263c60628e732dd05030955be3354d6f2
wangxuan127 <wangxuan127@jd.com>
2024-02-05 19:19:16 +0800

    edit

--- gltf.js
+++ gltf.js
@@ -7,6 +7,13 @@ const model = new THREE.Group()
 
 loader.load('./gltf/20221009151307_parent_directory_办公楼06.gltf', function(gltf) {
   model.add(gltf.scene)
+  gltf.scene.traverse(o => {
+    if(o.isMesh) {
+      o.material = new THREE.MeshLambertMaterial({
+        color: 0xffff00
+      })
+    }
+  })
 })
 
 export default model
